4f6584cf8d237ab8c32393f829238af1b0455be8
rakotonirinsarisoa <53931485+rakotonirinsarisoa@users.noreply.github.com>
2020-09-25 10:48:38 +0200

    teste

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,25 @@
-const text: string = 'ito!!!'
-console.log(text);
+// const text: string = 'ito!!!';
+console.log();
+var matTab = [
+    [0, 1, 0],
+    [1, 1, 0],
+    [0, 0, 1],
+];
+for (let i = 0; i < matTab.length; i++) {
+    // const element = array[index];
+    console.log(matTab);
+}
+//typescrypte
+var tab = [0, 3, -5, 12, -18, -20];
+var a = 0;
+for (let i = 0; i < tab.length; i++) {
+    if (tab[i] > 0) {
+        a = a + tab[i];
+    }
+}
+console.log('La Somme des entiter positif est:' + a);
+
+tab.sort(function (a, b) {
+    return a - b;
+});
+console.log(tab);
